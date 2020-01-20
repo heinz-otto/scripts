a9193404e9286c6f57aeed3025874d5de1a0fc23
Otto <heinz-otto.klas@gmx.de>
2020-01-20 11:09:18 +0100

    Update WinPEmount.cmd

--- batch/WinPEmount.cmd
+++ batch/WinPEmount.cmd
@@ -1,8 +1,7 @@
 @echo off
 if "%1"=="" (set opt=/discard) else (set opt=/commit)
 if /i "%1"=="c" (set opt=/commit) else (set opt=/discard)
-if "%2"=="" (set image=E:) else (set image=%2)
-REM if "%2"=="" (set image=D:\WinPE_amd64\media) else (set image=%2)
+if "%2"=="" (set image=D:\WinPE_amd64\media) else (set image=%2)
 if "%3"=="" (set mount=D:\WinPE_amd64\mount) else (set mount=%3)
 
 Dism /Mount-Image /ImageFile:%image%\sources\boot.wim /index:1 /MountDir:%mount%
@@ -16,7 +15,6 @@ echo @set drives=Z Y X W V U T S R Q P O N M L K J I H G F E D C
 echo @for %%%%a in ^(%%drives%%^) do @if exist %%%%a:%%pattern%% set DRIVE=%%%%a
 echo.@if %%DRIVE%% NEQ no @start %%DRIVE%%:%%pattern%%
 ) >> %mount%\Windows\System32\Startnet.cmd
-REM ) >> Startnet.cmd
 )
 pause 
 Dism /Unmount-Image /MountDir:%mount% %opt%
