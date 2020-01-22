513650e349891fbb5ffd772153d0231bc85b2b36
Otto <heinz-otto.klas@gmx.de>
2020-01-22 12:05:45 +0100

    Delete makerecovery.cmd

--- batch/makerecovery.cmd
+++
@@ -1,8 +0,0 @@
-@if "%1" == "" goto usage 
-md %1
-copy %2\System32\Recovery\winre.wim %1\winre.wim
-%2\System32\reagentc /setreimage /path %1 /target %2
-goto end
-:usage
-@echo MakeRecovery R:\Recovery\WindowsRE W:\Windows
-:end
