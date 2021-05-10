66a490f6c43454aad58f4a69521b93b0b85fa481
Otto <heinz-otto.klas@gmx.de>
2021-05-10 08:50:46 +0200

    Update 99_roborockUtils.pm

--- fhem/99_roborockUtils.pm
+++ fhem/99_roborockUtils.pm
@@ -8,15 +8,16 @@ package main;
 
 use strict;
 use warnings;
+use JSON;
 
 sub
-roborockUtils_Initialize 
-{
+roborockUtils_Initialize {
   my $hash = shift;
   return;
 }
 # Enter you functions below _this_ line.
 
+# strip the names for spots and zones and return a list 
 sub valetudoREdest {
 my $EVENT = shift;
 my ($text,%h);
@@ -31,34 +32,26 @@ for ('spots','zones') {
  return \%h
 }
 
+# return the last part topic and payload for mqtt message for certain custom_commands
 sub valetudoRE {
 my $EVENT = shift;
 my $ret = 'error';
 my ($cmd,$load) = split(q{ }, $EVENT,2);
-my $topic = 'valetudo/rockrobo';
-my (@zid,@l,%consum);
+# my $topic = ReadingsVal($NAME,'devicetopic','valetudo/rockrobo');
 if (@_) {Log 1,"sub valetudoRE - Befehl:$cmd Load:$load";return q{}}
+my (@zid,@l,%consum);
 
 if ($cmd eq 'zoned_cleanup') {@zid = split q{,},$load}
 if ($cmd eq 'map') {@l = split q{ },$load}
 for (qw(main_brush_work_time side_brush_work_time filter_work_time sensor_dirty_time))
     {$consum{(split q{_})[0]}=$_};
-	 
+
  my %Hcmd = (
     goto =>     { command => 'go_to',spot_id => $load },
     get_dest => { command => 'get_destinations' },
     map =>      { command => $l[0].'_map',name => $l[1] },
 	reset_consumable => { command => 'reset_consumable',consumable => $consum{$load} },
     zone =>     { command => 'zoned_cleanup',zone_ids => \@zid },
-    store_map => 
-  {
-    command => 'store_map',
-    name => $load
-  },load_map => 
-  {
-    command => 'load_map',
-    name => $load
-  },
   );
  if ($cmd eq 'x_raw_payload') {$ret=$load}
  else {$ret = toJSON $Hcmd{$cmd}}
