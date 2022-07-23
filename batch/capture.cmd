794da824f1eafd69820909b3768eb5dc30a6060c
Otto <heinz-otto.klas@gmx.de>
2022-07-23 12:27:15 +0200

    Update capture.cmd

--- batch/capture.cmd
+++ batch/capture.cmd
@@ -1,8 +1,9 @@
 @echo off & setlocal
 REM Diese Script erstellt von einer Windows System Disk ein wim Image
 REM Ein laufwerk mit dem Ordner \wim muss existieren
-REM setze den Imagenamen
+REM setze den Imagenamen auf backup oder Aufruf Argument
 set image=backup.wim
+if not "%1"=="" set image=%1.wim
 REM finde die Laufwerke anhand von Pfaden, der niedrigste Buchstabe wir gefunden
 call findelw.cmd \windows
 set cdir=%drive%
