5417b71013531266d3c8b5191df6d198aeea341b
Otto <heinz-otto.klas@gmx.de>
2022-09-25 18:34:06 +0200

    Create keepwg.sh

---
+++ Bash/keepwg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# entry in crontab: sudo crontab -e
+#*/5 * * * * /home/otto/keepwg.sh >> /home/otto/keepwg.log 2>&1
+if [ -z "$1" ]; then
+  echo "usage:"
+  echo "${0##*/} <Tunnel IP Server> [wireguard interface name]"
+  echo "${0##*/} 10.6.0.1 # Tunnel IP from wg server"
+  echo "${0##*/} 10.6.0.1 wg1 # other Interface than wg0 (default)"
+  exit
+fi
+
+WG_GWIP=$1
+WG_IF=WG_IF=${2:-"wg0"}
+date
+if ! /bin/ping -c 1 $WG_GWIP
+then
+  /usr/bin/systemctl restart wg-quick@${WG_IF}
+  echo "restart interface $WG_IF"
+  else echo "$WG_GWIP is reachable"
+fi
