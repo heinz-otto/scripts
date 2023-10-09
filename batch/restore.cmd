2b55987be81b0d79cd459bb15b900543df67195f
Otto <heinz-otto.klas@gmx.de>
2023-10-09 14:52:34 +0200

    Update restore.cmd

--- batch/restore.cmd
+++ batch/restore.cmd
@@ -2,8 +2,10 @@
 REM Diese Script stellt eine Windows System Disk komplett mit Neupartitionierung und Image wieder her
 REM image auswaehlen
 set image=backup.wim
+if not "%1"=="" set image=%1.wim
 REM Setze das Partitionsschema BIOS oder UEFI
 set PART=UEFI
+if not "%2"=="" set PART=%2
 
 @echo list disk|diskpart
 @echo Welche Disk soll neu gemacht werden?
@@ -29,6 +31,8 @@ set wim=%drive%:\wim
 REM diskpart /s CreatePartitions-%PART%.txt
 REM Bediene diskpart über Pipeline und ersetze select disk 0 durch eingegebene Disknummer
 echo Erzeuge Partitionen auf Disk %nummer% im %PART% Partition Style
+echo Druecke eine Taste fuer weiter oder ctrl+c fuer abbruch
+pause
 (echo sel disk %nummer% & type CreatePartitions-%PART%.txt|find /v /i "select disk 0")|find /v /i "rem "|diskpart
 echo restore von Image: %wim%\%image%
 dism /Apply-Image /ImageFile:%wim%\%image% /Index:1 /ApplyDir:W:\
@@ -38,5 +42,6 @@ echo erzeuge Recovery auf Laufwerk R:
 md R:\Recovery\WindowsRE
 copy %wim%\winre.wim R:\Recovery\WindowsRE\winre.wim
 W:\Windows\System32\reagentc /setreimage /path R:\Recovery\WindowsRE /target W:\Windows
-
+echo Druecke eine Taste für shutdown
+pause
 wpeutil shutdown
