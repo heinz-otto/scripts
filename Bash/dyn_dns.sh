030c93e42c4290f0f7f92282b811b2cbd78f4b74
Otto <heinz-otto.klas@gmx.de>
2025-09-16 20:10:09 +0200

    Update dyn_dns.sh

--- Bash/dyn_dns.sh
+++ Bash/dyn_dns.sh
@@ -7,7 +7,7 @@
 # entry in crontab: sudo crontab -e
 # */5 * * * * /usr/local/bin/dyn_dns.sh >> /var/log/dyn_dns.log 2>&1
 # */5 * * * * /usr/local/bin/dyn_dns.sh >/dev/null 2>&1
-ip_current=$(cat ip_current)
+ip_current=$(cat /tmp/ip_current)
 ip=$(dig @resolver4.opendns.com myip.opendns.com +short)
 if [ "$ip" != "$ip_current" ]; then
     echo 'reregister ddns for sub.domain.tld'
