35b7e1504681d7c98a2fe8cce17590080ae3daaa
Otto <heinz-otto.klas@gmx.de>
2025-09-16 19:37:37 +0200

    Create dyn_dns.sh

---
+++ Bash/dyn_dns.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# this script will ping the server side tunnel interface and restart the local interface if failes. 
+# file=dyn_dns.sh
+# wget -4 -O ${file} https://raw.githubusercontent.com/heinz-otto/scripts/master/Bash/${file}
+# chmod +x ${file}
+# sudo cp ${file} /usr/local/bin/
+# entry in crontab: sudo crontab -e
+# */5 * * * * /usr/local/bin/dyn_dns.sh >> /var/log/dyn_dns.log 2>&1
+# */5 * * * * /usr/local/bin/dyn_dns.sh >/dev/null 2>&1
+ip_current=$(cat ip_current)
+ip=$(dig @resolver4.opendns.com myip.opendns.com +short)
+if [ "$ip" != "$ip_current" ]; then
+    echo 'reregister ddns'
+    curl -su dynUser:password https://dyndns.kasserver.com
+    echo $ip>ip_current
+  else
+    echo 'ip not changed'
+fi
